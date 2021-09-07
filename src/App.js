e0071bc05d4fe52cacf9aa1a8aaf130c6b0b36ac
Nedal1994 <nedal_al_saleh94@hotmail.com>
2021-09-07 23:32:09 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 import React from 'react'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card'
 
 class App extends React.Component
 {
@@ -14,56 +17,43 @@ class App extends React.Component
       displayError:false,
 
       description: '',
-      description1: '',
-      description2: '',
-
       maxTemp:'',
-      maxTemp1:'',
-      maxTemp2:'',
-
-      minTemp:'',
-      minTemp1:'',
-      minTemp2:'',
-      
+      minTemp:'', 
       date: '',
-      date1: '',
-      date2: ''
+      weatherData:[]
     }
   }
   getLocationData = async(event) =>
   {
     event.preventDefault()
     const cityName = event.target.cityName.value
-    const myKey = `pk.5f176f6f3dac23ab3b25be92da1e89e4`
-    const URL = `https://us1.locationiq.com/v1/search.php?key=${myKey}&q=${cityName}&format=json`
-    const weatherUrl = `https://lap7.herokuapp.com/weather?city_name=${cityName}`
- 
-    const jsonData = await axios.get(weatherUrl)
-   console.log(jsonData.data);
+    const URL = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_KEY}&q=${cityName}&format=json`
+    // const weatherUrl = `https://lap7.herokuapp.com/weather?city_name=${cityName}`
+    const weatherUrl = `http://localhost:3200/weather?city_name=${cityName}&lat=${this.state.lat}&lon=${this.state.lon}`
+    
+console.log('ok')
     try{
+
       let result = await axios.get(URL)
-    this.setState({
-      date: jsonData.data[0].date,
-      date1: jsonData.data[1].date,
-      date2: jsonData.data[2].date,
+      console.log(result)
+      this.setState({
+      // date: jsonData.data[0].date,
+      // maxTemp:jsonData.data[0].maxTemp,
+      // minTemp:jsonData.data[0].minTemp,
+      // description: jsonData.data[0].description,
 
-      maxTemp:jsonData.data[0].maxTemp,
-      maxTemp1:jsonData.data[1].maxTemp,
-      maxTemp2:jsonData.data[2].maxTemp,
-
-      minTemp:jsonData.data[0].minTemp,
-      minTemp1:jsonData.data[1].minTemp,
-      minTemp2:jsonData.data[2].minTemp,
-
-      description: jsonData.data[0].description,
-      description1: jsonData.data[1].description,
-      description2: jsonData.data[2].description,
 
       lat:result.data[0].lat,
       lon:result.data[0].lon,
       displayName:result.data[0].display_name,
       mapFlag : true,
     })
+        const jsonData = await axios.get(weatherUrl)
+
+   this.setState({
+     weatherData:jsonData.data
+   })
+   console.log(this.state.weatherData);
   }
 
 
@@ -73,7 +63,6 @@ catch
     displayError:true
   })
 }
-console.log(this.state.maxTemp);
 }
 
 
@@ -85,22 +74,28 @@ console.log(this.state.maxTemp);
       <header>
         <h1>City Explorer</h1>
         </header><br/>
-      <form onSubmit={this.getLocationData}>
+      <Form onSubmit={this.getLocationData}>
         <label>City name</label><br/>
         <input type='text' name='cityName' id='citytext'/><br/><br/>   
-        <button type='submit'>Get location</button>
-      </form>
+        <Button type='submit'>Get location</Button>
+      </Form>
 
       <p>Display location name: {this.state.displayName}</p>
       <p>Latitude: {this.state.lat}</p>
       <p>Longitude: {this.state.lon}</p>
-      <p>Dates: <br/>{this.state.date}<br/>{this.state.date1}<br/>{this.state.date2}</p>
-      <p>Description: <br/>{this.state.description}<br/>{this.state.description1}<br/>{this.state.description2}</p>
-      <p>Temperatures: <br/>{this.state.minTemp} - {this.state.maxTemp}
-      <br/>{this.state.minTemp1} - {this.state.maxTemp1}<br/>
-      {this.state.minTemp1} - {this.state.maxTemp1}</p>
+      <p>Dates: <br/>{this.state.date}</p>
+      {/* <p>Description: <br/>{this.state.description}</p> */}
+      <img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_KEY}&center=${this.state.lat},${this.state.lon}&zoom=18&size=500x500&format=png&maptype=<MapType>&markers=icon:<icon>|<latitude>,<longitude>&markers=icon:<icon>|<latitude>,<longitude>`}/>
+      
+      {this.state.weatherData.map((item) => {
+        return (<Card>
+  <h1>{item.date}</h1>
+  <p>{item.description}</p>
+</Card>)
 
-      <img src={`https://maps.locationiq.com/v3/staticmap?key=pk.5f176f6f3dac23ab3b25be92da1e89e4&center=${this.state.lat},${this.state.lon}&zoom=18&size=500x500&format=png&maptype=<MapType>&markers=icon:<icon>|<latitude>,<longitude>&markers=icon:<icon>|<latitude>,<longitude>`}/>
+
+      })} 
+      
       <footer>
         &copy;Nedal Al Saleh
       </footer>
